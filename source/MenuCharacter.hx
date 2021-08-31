2e4db615cd4110295a63fa8dc49dad4e6977348b
neontflame <animationsneon@gmail.com>
2021-08-31 08:58:17 -0300

    big shits
    I take Big Ploopy

---
+++ source/MenuCharacter.hx
@@ -0,0 +1,79 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+class CharacterSetting
+{
+	public var x(default, null):Int;
+	public var y(default, null):Int;
+	public var scale(default, null):Float;
+	public var flipped(default, null):Bool;
+
+	public function new(x:Int = 0, y:Int = 0, scale:Float = 1.0, flipped:Bool = false)
+	{
+		this.x = x;
+		this.y = y;
+		this.scale = scale;
+		this.flipped = flipped;
+	}
+}
+
+class MenuCharacter extends FlxSprite
+{
+	private static var settings:Map<String, CharacterSetting> = [
+		'bf' => new CharacterSetting(0, 0, 1, true),
+		'gf' => new CharacterSetting(190, -70, 1, true),
+		'dad' => new CharacterSetting(160, -70, 1, true),
+		'spooky' => new CharacterSetting(170, -70, 1, true),
+		'pico' => new CharacterSetting(160, -70, 1, true),
+		'mom' => new CharacterSetting(-30, 140, 0.85),
+		'parents-christmas' => new CharacterSetting(100, 130, 1.8),
+		'senpai' => new CharacterSetting(-40, -45, 1.4)
+	];
+
+	private var flipped:Bool = false;
+
+	public function new(x:Int, y:Int, scale:Float, flipped:Bool)
+	{
+		super(x, y);
+		this.flipped = flipped;
+
+		antialiasing = false;
+
+		frames = Paths.getSparrowAtlas('storymenuChar');
+
+		animation.addByPrefix('bf', "BF idle dance white", 24);
+		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
+		animation.addByPrefix('gf', "tutorial", 24);
+		animation.addByPrefix('dad', "n1", 24);
+		animation.addByPrefix('spooky', "n2", 24);
+		animation.addByPrefix('pico', "cola", 24);
+		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
+		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
+		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
+
+		setGraphicSize(Std.int(width * scale));
+		updateHitbox();
+	}
+
+	public function setCharacter(character:String):Void
+	{
+		if (character == '')
+		{
+			visible = false;
+			return;
+		}
+		else
+		{
+			visible = true;
+		}
+
+		animation.play(character);
+
+		var setting:CharacterSetting = settings[character];
+		offset.set(setting.x, setting.y);
+		setGraphicSize(Std.int(width * setting.scale));
+		flipX = setting.flipped != flipped;
+	}
+}
